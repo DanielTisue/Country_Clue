2189a8416223fd715abc64c102bcd60e3a6ea424
DanielTisue <d.tisue@gmail.com>
2021-03-14 12:13:18 -0500

    Created Form, Form style, Post, Post style, and Posts with Posts styles to client side. Added quick logo .png filler - may not be final. Landing still needs a lot of styling but skeletal ground work is established.

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,35 @@
 import React from 'react';
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+
+import Posts from './components/Posts/Posts';
+import Form from './components/Form/Form';
+
+import guitar from './images/guitar.png'
+import useStyles from './styles';
 
 const App = () => {
+  const classes = useStyles();
   return (
-    <div>
-      <h1>App</h1>
-    </div>
+      <Container maxwidth="lg">
+        <AppBar className={classes.appBar} position="static" color="inherit">
+        <img className={classes.image} src={guitar} alt="guitar" align="center" height="50" />
+        <Typography className={classes.heading} variant="h3" align="center">Country Clue</Typography>
+        
+        </AppBar>
+
+        <Grow in>
+          <Container>
+            <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+              <Grid item xs={12} sm={7}>
+                <Posts />
+              </Grid>
+              <Grid item xs={12} sm={4}>
+                <Form />
+              </Grid>
+            </Grid>
+          </Container>
+        </Grow>
+      </Container>
   );
 }
 
---
+++ client/src/styles.js
@@ -0,0 +1,19 @@
+import { makeStyles } from '@material-ui/core';
+
+export default makeStyles(() => ({
+  appBar: {
+    borderRadius: 15,
+    margin: '30px 0',
+    padding: '10px',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  heading: {
+    color: 'rgba(0,183,255, 1)',
+  },
+  image: {
+    marginRight: '15px',
+  },
+}));
