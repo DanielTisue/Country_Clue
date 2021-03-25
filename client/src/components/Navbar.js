b6d709ddd36290ffa57cdf9c91d9eb9c5bc97ec6
DanielTisue <d.tisue@gmail.com>
2021-03-25 08:05:35 -0500

    Started Navbar build buy adding links.

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -4,7 +4,13 @@ class Navbar extends React.Component {
   render() {
     return (
       <div>
-
+        <ul>
+          <li className="navabar-link">Home</li>
+          <li className="navabar-link">About</li>
+          <li className="navabar-link">The Latest</li>
+          <li className="navabar-link">New Artist</li>
+          <li className="navabar-link">Roadtrip Must Playlist</li>
+        </ul>
       </div>
     )
   }
