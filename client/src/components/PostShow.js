a069fbe896a7c017eccf25c189f327a64ac764de
DanielTisue <d.tisue@gmail.com>
2021-04-15 07:59:47 -0500

    Adjusted some styles

--- client/src/components/PostShow.js
+++ client/src/components/PostShow.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 // import { Link } from 'react-router-dom';
 import './PostShow.css';
 // import heart from '../Images/LikeCount.svg';
-import tagImage from '../Images/tag.svg';
+// import tagImage from '../Images/tag.svg';
 
 class PostShow extends Component {
   constructor(props) {
