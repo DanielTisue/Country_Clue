38e365999047244e970b93882b8451f6b892984e
DanielTisue <d.tisue@gmail.com>
2021-05-15 10:51:51 -0500

    Attempted sort method to PostList array. Current implementation tested and failed. Method commented out.

--- client/src/components/PostList.js
+++ client/src/components/PostList.js
@@ -3,31 +3,29 @@ import axios from 'axios';
 import Post from './Post';
 
 class PostList extends React.Component {
-
-
   state = {
     posts: []
   }
+
   componentDidMount() {
     this.getPosts();
   }
 
   async getPosts() {
     const res = await axios.get("http://localhost:5000/posts/");
-    this.setState({posts: res.data});
+    // const myData = [].concat(this.state.data)
+    // .sort((a, b) => a.itemM > b.itemM ? 1 : -1)
+    this.setState({posts: res.data})
   }
 
 
   renderList() {
     return this.state.posts.map( post => {
     return <Post post={post} key={post._id} />
-  });
+    });
   }
 
 render(){
-//  return this.state.posts.map( post => {
-//     return <Post post={post} key={post._id} />
-//   });
   return <div className="container">
             <div className="flex-container">
               {this.renderList()}
