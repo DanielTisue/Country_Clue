6cdd1aaa0067ea026eeb31161a2052d4dcd354a1
DanielTisue <d.tisue@gmail.com>
2021-05-26 22:08:15 -0500

    Cleaned up App.js and moved routes to Router.js. Added conditional logic to protect access for specific routes.

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,8 @@
 import React from 'react';
 import { AuthContextProvider } from './components/Context/AuthContext';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Home from './components/Home';
-import PostList from './components/Posts/PostList';
-import ShowPost from './components/Posts/ShowPost';
-import CreatePostForm from './components/PostForms/CreatePostForm';
-import PostEditForm from './components/PostForms/PostEditForm';
-import Register from './components/Auth/Register';
-import Login from "./components/Auth/Login";
+import Router from './Router'
 import './style.css';
-import Navbar from './components/Navbar';
+
 
 
 
@@ -19,19 +12,7 @@ class App extends React.Component{
   render(){
     return (
       <AuthContextProvider>
-        
-      <BrowserRouter>
-      <Navbar />
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/posts/:id" component={ShowPost} />
-          <Route exact path="/posts/:id/edit" component={PostEditForm} />
-          <Route exact path="/posts" component={PostList} />
-          <Route exact path="/create" component={CreatePostForm} />
-          <Route exact path="/auth/register" component={Register} />
-          <Route exact path="/auth/login" component={Login} />
-        </Switch>
-      </BrowserRouter>
+        <Router />
       </AuthContextProvider>
     )
   }
