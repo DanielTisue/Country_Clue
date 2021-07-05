ceed7cf8f5921dbb49dd92daa574ecbb9c4eadc0
DanielTisue <d.tisue@gmail.com>
2021-07-04 20:29:54 -0500

    Added featured section and an about section.

--- client/src/components/Layout/Home.js
+++ client/src/components/Layout/Home.js
@@ -5,9 +5,17 @@ class Home extends React.Component {
     return (
      <div className="container"> 
         <div className="homepage-title-wrapper">
-
           <h1 id="homepage-title">Homepage</h1>
         </div>
+        <section className="featured">
+          <h1 id="featured-section">The Latest</h1>
+        </section>
+
+        <section className="about">
+          <h1 id="about-section">What are we about?</h1>
+        </section>
+
+        
     </div>
       )
   }
