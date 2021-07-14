bd598c46c1301108bc5face875fd319a470b4732
DanielTisue <d.tisue@gmail.com>
2021-07-13 20:05:39 -0500

    Started landing page design. Added SVG background and change type font.

--- client/src/components/Layout/Home.js
+++ client/src/components/Layout/Home.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import {ReactComponent as Background } from '../Styles/MusicBackground.svg'
 
 class Home extends React.Component {
   render() {
     return (
-     <div className="container"> 
-        <div className="homepage-title-wrapper">
-          <h1 id="homepage-title">Homepage</h1>
-        </div>
+      <React.Fragment>
+         <Background className="background-img" />
+        <div className="container"> 
+          <div className="homepage-title-wrapper">
+            <h1 id="homepage-title">Gone Country</h1>
+            <h2 id="subhead">Get the latest in country music</h2>
+          
+          </div>
+
         <section className="featured">
           <h1 id="featured-section">The Latest</h1>
         </section>
@@ -16,6 +22,7 @@ class Home extends React.Component {
         </section>
         
     </div>
+    </React.Fragment>
       )
   }
 }
