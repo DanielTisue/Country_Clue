97fb959192de811e5aa0a546e302a8402a0e10f6
DanielTisue <d.tisue@gmail.com>
2021-07-30 08:17:01 -0500

    Set error handling.

--- client/src/components/Auth/LogOutBtn.js
+++ client/src/components/Auth/LogOutBtn.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import axios from 'axios';
 import AuthContext from '../Context/AuthContext';
 import { useHistory } from 'react-router-dom';
@@ -7,17 +7,31 @@ function LogOutBtn() {
 
   const { getLoggedIn } = useContext(AuthContext);
 
+  const [error, setError] = useState(null);
+
   let history = useHistory();
 
   async function logOut() {
+    try {
       await axios.get('http://localhost:5000/auth/logout');
       await getLoggedIn();
       history.push('/');
+    }
+    catch(err) {
+       if (err.response.status === 500) {
+        setError('Status: 500 - There is a problem with the server. Please contact your site admin')
+      } 
+      setError(err.response.data)
+    }
   }
   return (
+    <React.Fragment>
+      { error && <div className="error-message-wrapper"><div className="error-message">{ error }</div></div> }
     <button className="secondary" onClick={logOut}>
       Log out
     </button>
+    </React.Fragment>  
+     
   )
 }
 
