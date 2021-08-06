ead735af2614ca624fd7b13e6b6c8e6a833dd941
DanielTisue <d.tisue@gmail.com>
2021-08-05 21:57:33 -0500

    Added menu tab slider. Fixed hamburger menu animation.

--- client/src/components/Layout/Navbar.js
+++ client/src/components/Layout/Navbar.js
@@ -1,31 +1,35 @@
-import React, { useState, useContext, useRef } from "react";
+import React, { useState, useContext } from "react";
 import { Link } from 'react-router-dom';
 import AuthContext from "../Context/AuthContext";
 import LogOutBtn from '../Auth/LogOutBtn';
 import {ReactComponent as HomeLogo } from '../../Assets/Images/guitar_amp.svg';
-import {ReactComponent as Articles } from '../../Assets/Images/articles.svg';
-import {ReactComponent as CreateArticle } from '../../Assets/Images/file-plus.svg';
+// import {ReactComponent as Articles } from '../../Assets/Images/articles.svg';
+// import {ReactComponent as CreateArticle } from '../../Assets/Images/file-plus.svg';
 
 
 function Navbar() {
 const { loggedIn } = useContext(AuthContext);
 
+const [open, setOpen] = useState(false);
 
+const showOpen = () => setOpen(!open);
 
-
-    return (
-      // <div className="container">
+return (
         <div className="navBar">
+
           <div className="navBar-link">
           <Link to='/'>
             
                 <HomeLogo className="navBar-home-icon" alt="HomeLogo" />             
             </Link>
           </div>
-          
-
-          {/* <ul className="navBar-items">
-            
+        
+       <div className="menu-bar-wrapper">
+          <div className={open ? 'hamburger open' : 'hamburger'} onClick={showOpen}>
+            <div id="rotate-bottom" className="line"></div>
+          </div>
+         <div className="menu-bar">
+            <ul className="navBar-items">
             <Link to='/'>
             <li className="navBar-link-item">About</li>
             </Link>
@@ -36,32 +40,28 @@ const { loggedIn } = useContext(AuthContext);
             </Link>
 
             <Link to='/posts' >
-            <li className="navBar-link-item"> <Articles className="navBar-articles-icon menu-icon" alt="Articles" /><span className="menu-text">Articles</span></li>
-            </Link> */}
-
-            {/* {!loggedIn && (
-              <Link to='/auth/login' >
-              <li className="navBar-link">Login</li>
-              </Link>
-            )} */}
+            <li className="navBar-link-item"> Articles</li>
+            </Link>
             
-            {/* {loggedIn && (
+           {loggedIn && (
              <Link to='/create' >
-              <li className="navBar-link"><CreateArticle className="navBar-create-icon menu-icon" alt="create Article" /><span className="menu-text">Create Article</span></li>
+              <li className="navBar-link-item">Create Article</li>
+              </Link>
+            )}
+
+            {!loggedIn && (
+              <Link to='/auth/login' >
+              <li className="navBar-link-item">Login</li>
               </Link>
             )}
 
             {loggedIn && (
-              <li className="navBar-link"> <LogOutBtn /> </li>
+              <li className="navBar-link-item"> <LogOutBtn /> </li>
             )}
-            
-          </ul> */}
-        
-          <div className="hamburger" >
-            <div id="rotate-bottom" className="line"></div>
-          </div>
-        </div>
-      //  </div>
+          </ul>
+         </div>
+       </div>  
+    </div>
     )
   }
 
