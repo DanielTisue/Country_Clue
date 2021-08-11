95464f1cb5383ec9a7a876e7bdca51a74a9f9c08
DanielTisue <d.tisue@gmail.com>
2021-08-11 07:42:11 -0500

    Added footer component.

---
+++ client/src/components/Layout/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div className="footer-container">
+      <div className="footer">
+      <p>This is the footer with a copyright and stuff &copy;</p>
+      </div>
+    </div>
+  )
+}
